55e60c6ab6fb2b123204d1040e893c1327de3a62
Kirsten Birch Håkansson, DTU Aqua <kih@aqua.dtu.dk>
2026-03-10 11:24:46 +0100

    New Danish data

--- taf_setup_run_her.27.20-24_sc.R
+++ taf_setup_run_her.27.20-24_sc.R
@@ -69,6 +69,15 @@ draft.data(data.files = "submitted_data/DC_Annex_HAWG3 her.27.20-24 template_DNK
            file = T,
            append = T)
 
+draft.data(data.files = "submitted_data/DC_Annex_HAWG3 her.27.20-24 template_DNK_2025_v3.xlsx",
+           data.scripts = NULL,
+           originator = "Corrected by kibi",
+           title = "Had forgotten to delete number sampled in some unsampled strata. Correct a wrong fleet in one of the area 23 sheets",
+           period = "2025",
+           access = "Restricted",
+           file = T,
+           append = T)
+
 draft.data(data.files = "submitted_data/SE_2026 DC HAWG her.27.20-24 YellowSheet_v2.xlsx",
            data.scripts = NULL,
            originator = "Data submitted to HAWG",
