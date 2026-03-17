7c2a143b94165591e721f927e14e9cfbcd15510f
Kirsten Birch Håkansson, DTU Aqua <kih@aqua.dtu.dk>
2026-03-17 15:47:23 +0100

    Copy from 2025

---
+++ boot/download_from_stockassessment_org_single_fleet.R
@@ -0,0 +1,55 @@
+
+# devtools::install_github("fishfollower/SAM/stockassessment", ref="multi")
+
+library(stockassessment)
+
+SAOAssessment <- "WBSS_HAWG_2025_sf"   # = stock name in stockassesssment.org
+user <- 278                            # User 2 = Anders; User 3 = Guest (ALWAYS GETS THE LATEST COMMITTED VERSION)
+
+### Read raw data from stockassessment.org
+url <-
+  paste(
+    "https://www.stockassessment.org/datadisk/stockassessment/userdirs/user",
+    user,
+    "/",
+    SAOAssessment,
+    "/data/",
+    sep = ""
+  )
+
+filestoget <- c(
+  "cn.dat",
+  "cw.dat",
+  "dw.dat",
+  "lf.dat",
+  "lw.dat",
+  "mo.dat",
+  "nm.dat",
+  "pf.dat",
+  "pm.dat",
+  "sw.dat",
+  "survey.dat" #, "cn_A.dat", "cn_C.dat", "cn_D.dat", "cn_F.dat", "cw_A.dat", "cw_C.dat", "cw_D.dat", "cw_F.dat"
+)  #
+
+lapply(filestoget, function(f)
+    download.file(paste(url, f, sep = ""), f))
+
+### Read model from stockassessment.org
+url <-
+  paste(
+    "https://www.stockassessment.org/datadisk/stockassessment/userdirs/user",
+    user,
+    "/",
+    SAOAssessment,
+    "/run/",
+    sep = ""
+  )
+
+filestoget <- c(
+  "model.RData"
+)  #, "cn_A.dat", "cn_C.dat", "cn_D.dat", "cn_F.dat", "cw_A.dat", "cw_C.dat", "cw_D.dat", "cw_F.dat"
+
+
+  lapply(filestoget, function(f)
+    download.file(paste(url, f, sep = ""), f))
+
