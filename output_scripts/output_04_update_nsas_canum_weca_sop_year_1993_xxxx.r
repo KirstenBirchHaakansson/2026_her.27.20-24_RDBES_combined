d98168e3b9aa989cfad36f642f870bbdbf7a421b
Kirsten Birch Håkansson, DTU Aqua <kih@aqua.dtu.dk>
2026-04-08 20:47:47 +0200

    Update output_04_update_nsas_canum_weca_sop_year_1993_xxxx.r

--- output_scripts/output_04_update_nsas_canum_weca_sop_year_1993_xxxx.r
+++ output_scripts/output_04_update_nsas_canum_weca_sop_year_1993_xxxx.r
@@ -3,10 +3,11 @@ library(dplyr)
 library(knitr)
 library(readxl)
 
-year_last <- 2023
-year_to_update <- c(2024)
+year_to_update <- c(2000:2025)
+year_last <- 2024 #The end year of the time series to update
+year_new_last <- 2025
 
-dat_old <- read.csv(paste0("boot/data/time_series/", "nsas_canum_weca_sop_year_1993_", year_last, ".csv"))
+dat_old <- read.csv(paste0("boot/data/data_from_last_year/", "nsas_canum_weca_sop_year_1993_", year_last, ".csv"))
 
 names(dat_old)
 
@@ -23,9 +24,26 @@ kable(summarise(group_by(dat_old, year), nsas_canum_1000 = sum(nsas_canum_1000, 
 kable(summarise(group_by(dat_old_remove, year), nsas_canum_1000= sum(nsas_canum_1000, na.rm = T)),
       caption = "Old time series. Sum of CANUM to be updated")
 
+
+# BM
+dat_bm <-
+  read.csv(paste0(
+    "boot/data/data_from_bm/",
+    "30_updated_nsas_input_fleet_area_2000-2024.csv"
+  ))
+names(dat_bm)
+
+dat_bm_1 <- subset(dat_bm, (year %in% year_to_update) & area %in% c("27.3.a.20", "27.3.a.21"))
+
+unique(dat_bm_1$year)
+
+dat_bm_remove <- subset(dat_bm,!(year %in% year_to_update))
+
+unique(dat_bm_remove$year)
+
 # New data
 
-dat_new <- read.csv(paste0("data/", "C6_her2024_canum_wbss_nsas_", year_to_update, ".csv"))
+dat_new <- read.csv(paste0("data/", "21_C4_her2024_canum_wbss_nsas_", year_new_last, ".csv"), sep = ";")
 names(dat_new)
 
 dat_new <- subset(dat_new, area %in% c("27.3.a.20", "27.3.a.21"))
@@ -50,11 +68,14 @@ kable(summarise(group_by(dat_new_1, year), nsas_canum_1000 = sum(nsas_canum_1000
 # Areas are joined in the Old time series
 
 unique(dat_old_1$area)
+unique(dat_bm_1$area)
 unique(dat_new_1$area)
 
 dat_new_1$area[dat_new_1$area %in% c("27.3.a.20", "27.3.a.21")] <- "27.3.a"
+dat_bm_1$area[dat_bm_1$area %in% c("27.3.a.20", "27.3.a.21")] <- "27.3.a"
 
 unique(dat_new_1$area)
+unique(dat_bm_1$area)
 
 hist(dat_new_1$weca_g)
 
@@ -65,16 +86,27 @@ dat_new_sum <- summarise(group_by(dat_new_1, year, area, wr), nsas_canum_1000 = 
 
 dat_new_sum$weca_g <- dat_new_sum$catch_t/dat_new_sum$nsas_canum_1000
 
+dat_bm_1$sop <- dat_bm_1$nsas_canum_1000 * (dat_bm_1$weca_kg * 1000)
+
+dat_bm_sum <- summarise(group_by(dat_bm_1, year, area, wr), nsas_canum_1000 = sum(nsas_canum_1000, na.rm = T),
+                         catch_t = sum(sop, na.rm = T))
+
+dat_bm_sum$weca_g <- dat_bm_sum$catch_t/dat_bm_sum$nsas_canum_1000
+
 names(dat_old_1)
 names(dat_new_sum)
+names(dat_bm_sum)
 
-done <- rbind(ungroup(dat_old_1), ungroup(dat_new_sum))
+done <- rbind(ungroup(dat_old_1), ungroup(dat_new_sum), ungroup(dat_bm_sum))
 
 
 kable(summarise(group_by(done, year), nsas_canum_1000 = sum(nsas_canum_1000, na.rm = T)),
                 caption = "New time series")
 
+kable(summarise(group_by(done, year), nsas_canum_1000 = sum(nsas_canum_1000 * weca_g, na.rm = T)),
+      caption = "New time series")
 
-write.csv(done, paste0("output/", "nsas_canum_weca_sop_year_1993_", year_to_update, ".csv"), row.names = F)
+
+write.csv(done, paste0("output/", "nsas_canum_weca_sop_year_1993_", year_new_last, ".csv"), row.names = F)
 
 
